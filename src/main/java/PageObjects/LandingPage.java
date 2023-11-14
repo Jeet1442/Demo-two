6dab83b25b11a1d4352eb5dfbeb3cb2adf8cbcba
Debjeet Saha <saha.deb45618@gmail.com>
2023-11-14 18:24:36 +0530

    Third commit

---
+++ src/main/java/PageObjects/LandingPage.java
@@ -0,0 +1,43 @@
+package PageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import AbstractComponents.AbstractComponent;
+
+public class LandingPage extends AbstractComponent{
+	
+	WebDriver driver;
+	
+	public LandingPage(WebDriver driver) {
+		
+		super(driver);
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	@FindBy(id = "userEmail")
+	WebElement userEmail;
+	
+	@FindBy(id = "userPassword")
+	WebElement userPassword;
+	
+	@FindBy(id = "login")
+	WebElement login;
+	
+	public void LoginApplication(String email , String password) {
+		
+		userEmail.sendKeys(email);
+		userPassword.sendKeys(password);
+		login.click();
+	}
+	
+	public void goTO() {
+		
+		driver.get("https://rahulshettyacademy.com/client");
+	}
+
+}
---
+++ src/test/java/Package/Automation_eCommerce.java
@@ -0,0 +1,64 @@
+package Package;
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import PageObjects.LandingPage;
+
+public class Automation_eCommerce {
+
+	@Test
+	public void E_Comerce() {
+		
+		WebDriver driver= new FirefoxDriver();
+		//WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		LandingPage landing = new LandingPage(driver);
+		
+		landing.goTO();
+		landing.LoginApplication("saha.debjeet123@gmail.com", "Debjeet123");
+		
+		
+		
+		List<WebElement> items = driver.findElements(By.cssSelector(".col-md-6"));
+		
+		for(int i=0; i<items.size();i++) {
+			String name = driver.findElements(By.cssSelector("b")).get(i+1).getText();
+			System.out.println(name);
+			
+			if(name.equalsIgnoreCase("ZARA COAT 3")) {
+				driver.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+				break;
+			}
+		}
+		
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(7));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".ng-animating")));
+		
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(@routerlink,'cart')]")));
+		driver.findElement(By.xpath("//button[contains(@routerlink,'cart')]")).click();
+		
+		driver.findElement(By.xpath("//button[contains(text(),'Checkout')]")).click();
+		
+		driver.findElement(By.xpath("//input[@placeholder='Select Country']")).sendKeys("India");
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(@class,'ta-item')][2]")));
+		driver.findElement(By.xpath("//button[contains(@class,'ta-item')][2]")).click();
+		
+		driver.findElement(By.cssSelector(".action__submit ")).click();
+		
+		
+
+	}
+
+}
